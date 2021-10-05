ae06788053b514c1d31b41bcde7134ad0d14e213
phienhoang <90850020+phienhoang@users.noreply.github.com>
2021-10-05 21:48:50 +0300

    Add files via upload

---
+++ w5/hero.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# license: GPLv3
+from gameunit import *
+
+class Hero(Attacker):
+	def __init__(self,name):
+		self._health = 100
+		self._attack = 50
+		self._experience = 0
+		self._name = name
+
+	def attack(self,target):
+		target._health -= self._attack
+		self._experience += 10
+
+
+#FIXME:
+"""В этом файле должен быть описан класс героя, унаследованный от Attacker
+Герой должен иметь 100 поинтов здоровья, атаку 50, опыт 0, имя, задаваемое в конструкторе
+Метод attack должен получать атрибут target и уменьшать его здоровье на величину атаки.
+
+"""
